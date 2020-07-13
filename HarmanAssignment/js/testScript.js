60e2fcf4f1763b8722258b4113feddffd43af5ed
yaten1991 <40053460+yaten1991@users.noreply.github.com>
2020-07-13 21:03:25 +0530

    Update testScript.js

--- HarmanAssignment/js/testScript.js
+++ HarmanAssignment/js/testScript.js
@@ -5,8 +5,8 @@ setTimeout(() => {
 }, duration);
 
 async function unitTest() {
-  let btnElement = [...document.getElementsByClassName('btnClass')];
-  if (btnElement.length === buttonCount) {
+  let btnElements = [...document.getElementsByClassName('btnClass')];
+  if (btnElements.length === buttonCount) {
     console.log(`Test case PASS, All ${buttonCount} buttons are available on UI.`);
   } else {
     console.log(`Test case FAIL, All ${buttonCount} buttons are not available on UI.`);
